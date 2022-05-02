56554c51d53f6903c4ac3ff6337599ed92c1d86c
Karim Layoun <layounkarim@gmail.com>
2022-05-02 11:20:40 -0400

    dataclass general attribute

--- src/theClasses.py
+++ src/theClasses.py
@@ -2,19 +2,29 @@ from dataclasses import dataclass
 import numpy as np
 from numpy.random import default_rng
 
+rng = default_rng()
+
 @dataclass
 class Asset():
     S: float
     vol: float
     drift: float
     market_impact: float
+    process: ...
+
+@dataclass
+class Trader():
+    risk_aversion: float
+    portfolio: int
 
 
 asset = Asset(
     100,
     0.3,
     0.02,
-    1.2
+    1.2,
+    rng.standard_normal
 )
 
 print(asset)
+print(asset.process())
