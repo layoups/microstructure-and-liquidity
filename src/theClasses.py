e6fc09a9c5fca9a1123936681dc9eafd5ba5605a
Karim Layoun <layounkarim@gmail.com>
2022-05-02 10:51:45 -0400

    asset class

--- src/theClasses.py
+++ src/theClasses.py
@@ -1,2 +1,20 @@
+from dataclasses import dataclass
 import numpy as np
 from numpy.random import default_rng
+
+@dataclass
+class Asset():
+    S: float
+    vol: float
+    drift: float
+    market_impact: float
+
+
+asset = Asset(
+    100,
+    0.3,
+    0.02,
+    1.2
+)
+
+print(asset)
