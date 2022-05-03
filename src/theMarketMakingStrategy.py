28d969ae6327d912d1587dd69103c0f1c092278c
Karim Layoun <layounkarim@gmail.com>
2022-05-03 01:22:26 -0400

    rolling inventory

--- src/theMarketMakingStrategy.py
+++ src/theMarketMakingStrategy.py
@@ -9,7 +9,7 @@ class MarketMaking:
         T: float,
         dt: float,
         M: int,
-        simulator: Callable[[float], int]
+        simulator: Callable[[int, float], int]
     ) -> None:
         self.trader = trader
         self.asset = asset
@@ -18,14 +18,14 @@ class MarketMaking:
         self.N = int(T / dt)
         self.T = self.N * self.dt
         self.M = M
-        self.Q: List[int] = [trader.portfolio]
-        self.X: List[float] = [trader.cash_account]
-        self.P: List[float] = [trader.cash_account + trader.portfolio * asset.S]
-        self.S: List[float] = [asset.S]
-        self.dSa: List[float] = []
-        self.dSb: List[float] = []
-        self.Na: List[int] = [0]
-        self.Nb: List[int] = [0]
+        self.Q: List[int] = np.concatenate(([trader.portfolio], np.zeros(self.N - 1)), axis=0)
+        self.X: List[float] = np.concatenate(([trader.cash_account], np.zeros(self.N - 1)), axis=0)
+        self.P: List[float] = np.concatenate(([trader.cash_account + trader.portfolio * asset.S], np.zeros(self.N - 1)), axis=0)
+        self.S: List[float] = np.concatenate(([asset.S], np.zeros(self.N - 1)), axis=0)
+        self.dSa: List[float] = np.zeros(self.N)
+        self.dSb: List[float] = np.zeros(self.N)
+        self.Na: List[int] = np.zeros(self.N)
+        self.Nb: List[int] = np.zeros(self.N)
         self.simulator = simulator
 
     def generate_quotes(self) -> tuple:
@@ -40,25 +40,28 @@ class MarketMaking:
         return temp1, temp2
 
     def make_markets(self) -> None:
-        for n in range(1, self.N + 1):
+        for n in range(1, self.N):
             quotes = self.generate_quotes()
             self.dSa[n-1] = quotes[0] + quotes[1]
             self.dSb[n-1] = quotes[0] - quotes[1]
 
             prob_a = self.asset.liquidity * np.exp(-self.kappa * self.dSa[n-1]) * self.dt
-            self.Na[n] = self.simulator(prob_a) if self.Q[n-1] > -self.M else 0
+            Na = self.simulator(1, prob_a) if self.Q[n-1] > -self.M else 0
+            self.Na[n] = self.Na[n-1] + Na
 
             prob_b = self.asset.liquidity * np.exp(-self.kappa * self.dSb[n-1]) * self.dt
-            self.Nb[n] = self.simulator(prob_b) if self.Q[n-1] < self.M else 0
+            Nb = self.simulator(1, prob_b) if self.Q[n-1] < self.M else 0
+            self.Nb[n] = self.Nb[n-1] + Nb
 
-            self.S[n] = self.asset.drift * self.dt \
-                + self.asset.vol * np.sqrt(self.dt) * self.asset.process() \
-                    + self.asset.market_impact * self.Na[n-1] \
-                        - self.asset.market_impact * self.Nb[n-1]
+            self.S[n] = self.S[n-1] \
+                + self.asset.drift * self.dt \
+                    + self.asset.vol * np.sqrt(self.dt) * self.asset.process() \
+                        + self.asset.market_impact * Na \
+                            - self.asset.market_impact * Nb
             self.Q[n] = self.Nb[n] - self.Na[n]
             self.X[n] = self.X[n-1] \
-                + (self.S[n] + self.dSa[n-1]) * self.Na[n] \
-                    - (self.S[n] - self.dSb[n-1]) * self.Nb[n]
+                + (self.S[n] + self.dSa[n-1]) * Na \
+                    - (self.S[n] - self.dSb[n-1]) * Nb
             self.P[n] = self.X[n] + self.Q[n] * self.S[n]
 
 
@@ -70,4 +73,30 @@ class MarketMaking:
 
 if __name__ == "__main__":
     rng = default_rng()
-    print(rng.binomial(1, 0.5))
+
+    asset = Asset(
+        100,
+        0.2,
+        0.1,
+        0,
+        140,
+        rng.standard_normal
+    )
+
+    trader = Trader(1e-1, 0, 0)
+
+    market_maker = MarketMaking(
+        trader,
+        asset,
+        1.5,
+        1,
+        0.005,
+        10,
+        rng.binomial
+    )
+
+    market_maker.make_markets()
+    print(market_maker.S)
+    # print(np.cumsum(market_maker.Na))
+    # print(np.cumsum(market_maker.Nb))
+    # print(np.cumsum(market_maker.Nb) - np.cumsum(market_maker.Na))
